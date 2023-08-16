1e94ebe03de7701014bd77d68c76c8f3f295c484
norld <grymore@gmail.com>
2023-08-16 18:28:37 +0800

    typewritter

--- wp-content/themes/tulisandaiyan/assets/script/index.js
+++ wp-content/themes/tulisandaiyan/assets/script/index.js
@@ -1,11 +1,10 @@
 const seconds = document.querySelector(".seconds .number"),
-    minutes = document.querySelector(".minutes .number"),
-    hours = document.querySelector(".hours .number"),
-    days = document.querySelector(".days .number");
+  minutes = document.querySelector(".minutes .number"),
+  hours = document.querySelector(".hours .number"),
+  days = document.querySelector(".days .number");
 
 const dateLaunch = new Date("2023-08-26");
 const dateNow = new Date();
-
 const differenceInMilliseconds = dateLaunch - dateNow;
 const calcseconds = Math.floor(differenceInMilliseconds / 1000) % 60;
 const calcminutes = Math.floor(differenceInMilliseconds / (1000 * 60)) % 60;
@@ -17,28 +16,27 @@ let secValue = calcseconds,
     dayValue = calcdays;
 
 const timeFunction = setInterval(() => {
-    secValue--;
+  secValue--;
 
-    if (secValue === 0) {
-        minValue--;
-        secValue = 60;
-    }
-    if (minValue === 0) {
-        hourValue--;
-        minValue = 60;
-    }
-    if (hourValue === 0) {
-        dayValue--;
-        hourValue = 24;
-    }
-
-    if (dayValue === 0) {
-        clearInterval(timeFunction);
-    }
-    seconds.textContent = secValue < 10 ? `0${secValue}` : secValue;
-    minutes.textContent = minValue < 10 ? `0${minValue}` : minValue;
-    hours.textContent = hourValue < 10 ? `0${hourValue}` : hourValue;
-    days.textContent = dayValue < 10 ? `0${dayValue}` : dayValue;
+  if (secValue === 0) {
+    minValue--;
+    secValue = 60;
+  }
+  if (minValue === 0) {
+    hourValue--;
+    minValue = 60;
+  }
+  if (hourValue === 0) {
+    dayValue--;
+    hourValue = 24;
+  }
+  if (dayValue === 0) {
+    clearInterval(timeFunction);
+  }
+  seconds.textContent = secValue < 10 ? `0${secValue}` : secValue;
+  minutes.textContent = minValue < 10 ? `0${minValue}` : minValue;
+  hours.textContent = hourValue < 10 ? `0${hourValue}` : hourValue;
+  days.textContent = dayValue < 10 ? `0${dayValue}` : dayValue;
 }, 1000); //1000ms = 1s
 
 
@@ -62,45 +60,34 @@ $('#lightSlider2').lightSlider({
 
 console.clear();
 const faqs = document.querySelectorAll('[data-list="faq"]');
-// console.log(faqs.length);
-
 if (faqs.length != 0) {
-    faqs.forEach(function (faq, index) {
-
-        // Check if a FAQ is changed
-        faq.addEventListener('change', function (e) {
-            const input = e.target;
-            const item = e.target.parentNode;
-            const label = e.target.nextElementSibling;
-            const type = e.target.getAttribute('type');
-
-            // Check if type is checkbox
-            if (type == 'checkbox') {
-
-                // console.log(label);
-                if (input.checked) {
-                    // console.log('checked');
-                    label.setAttribute('aria-expanded', 'true');
-                } else {
-                    // console.log('not checked');
-                    label.setAttribute('aria-expanded', 'false');
-                }
-            }
-
-            if (type == 'radio') {
-                console.log(type);
-            }
-
-
-
-        });
+  faqs.forEach(function (faq, index) {
+    // Check if a FAQ is changed
+    faq.addEventListener('change', function (e) {
+        const input = e.target;
+        const item = e.target.parentNode;
+        const label = e.target.nextElementSibling;
+        const type = e.target.getAttribute('type');
+        // Check if type is checkbox
+        if (type == 'checkbox') {
+          // console.log(label);
+          if (input.checked) {
+              // console.log('checked');
+              label.setAttribute('aria-expanded', 'true');
+          } else {
+              // console.log('not checked');
+              label.setAttribute('aria-expanded', 'false');
+          }
+        }
+        if (type == 'radio') {
+            console.log(type);
+        }
     });
-
+  });
 }
 
 var counted = 0;
 $(window).scroll(function() {
-
   var oTop = $('#counter').offset().top - window.innerHeight;
   if (counted == 0 && $(window).scrollTop() > oTop) {
     $('.count').each(function() {
@@ -128,27 +115,53 @@ $(window).scroll(function() {
     });
     counted = 1;
   }
-
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    const collapseButton = document.querySelector('[data-collapse-toggle="navbar-sticky"]');
-    const navbarMenu = document.getElementById('navbar-sticky');
-
-    collapseButton.addEventListener('click', function() {
-      navbarMenu.classList.toggle('hidden');
-      navbarMenu.classList.toggle('md:flex');
-    });
+  const collapseButton = document.querySelector('[data-collapse-toggle="navbar-sticky"]');
+  const navbarMenu = document.getElementById('navbar-sticky');
+  collapseButton.addEventListener('click', function() {
+    navbarMenu.classList.toggle('hidden');
+    navbarMenu.classList.toggle('md:flex');
   });
+});
 
-  $(document).ready(function () {
-    $(".nav ul a").on("click", function (e) {
-      e.preventDefault();
-
-      const href = $(this).attr("href");
-
-      $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
-    });
+$(document).ready(function () {
+  $(".nav ul a").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
   });
+});
 
-  
+
+const typeOne = document.getElementById('typeOne');
+const typeTwo = document.getElementById('typeTwo');
+
+const typewriterOne = new Typewriter(typeOne, {
+  loop: true, 
+  delay: 300,
+  deleteSpeed: 300,
+});
+
+const typewriterTwo = new Typewriter(typeTwo, {
+  loop: true,
+  delay: 130,
+  deleteSpeed: 130,
+});
+
+typewriterOne
+  .pauseFor(500)
+  .typeString('tulisan')
+  .pauseFor(4800)
+  .start();
+
+typewriterTwo
+  .pauseFor(670)
+  .typeString('by daiyan trisha')
+  .pauseFor(5000)
+  .start();
+
+
+  // 7 * 300
+  // 16 * 150
