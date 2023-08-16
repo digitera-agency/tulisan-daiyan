c315d4a9c4654c9bdb47cf9b386ec2c6bbabb157
jayaanggara <jayaanggara112@gmail.com>
2023-08-16 10:11:08 +0800

    update menu

--- wp-content/themes/tulisandaiyan/header.php
+++ wp-content/themes/tulisandaiyan/header.php
@@ -31,31 +31,31 @@
   <a href="/" class="flex items-center">
       <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/logo.png" class="h-8 mr-3" alt="Logo">
   </a>
-  <div class="flex md:order-2">
+  <div class="flex lg:order-2">
       <a href="#preorder" class="text-sm font-semibold text-white bg-[#8E2424] rounded-full px-6 me-3 py-2.5 tracking-widest">Pre-order now</a>
-      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
   </div>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul class="md:bg-[#FBF6F0] bg-[#282828] flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+  <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="navbar-sticky">
+    <ul class="lg:bg-[#FBF6F0] bg-[#282828] flex flex-col p-4 w-full lg:p-0 mt-4 font-medium border border-gray-100 rounded-lg lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0">
       <li>
-        <a href="#synopsis" class="block py-2 pr-4 uppercase text-white md:text-[#111]">synopsis</a>
+        <a href="#synopsis" class="block py-2 pr-4 uppercase text-white lg:text-[#111]">synopsis</a>
       </li>
       <li>
-        <a href="#package" class="block py-2 pr-4 uppercase text-white md:text-[#111]">Package</a>
+        <a href="#package" class="block py-2 pr-4 uppercase text-white lg:text-[#111]">Package</a>
       </li>
       <li>
-        <a href="#timeline" class="block py-2 pr-4 uppercase text-white md:text-[#111]">Timeline</a>
+        <a href="#timeline" class="block py-2 pr-4 uppercase text-white lg:text-[#111]">Timeline</a>
       </li>
       <li>
-        <a href="#faq" class="block py-2 pr-4 uppercase text-white md:text-[#111]">Faq</a>
+        <a href="#faq" class="block py-2 pr-4 uppercase text-white lg:text-[#111]">Faq</a>
       </li>
       <li>
-        <a href="#contact" class="block py-2 pr-4 uppercase text-white md:text-[#111]">Contact</a>
+        <a href="#contact" class="block py-2 pr-4 uppercase text-white lg:text-[#111]">Contact</a>
       </li>
     </ul>
   </div>
