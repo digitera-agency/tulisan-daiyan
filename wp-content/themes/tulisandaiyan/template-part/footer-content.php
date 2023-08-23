0d1efe20ab362d0b2ce7ef219b83df8df11325c2
norld <grymore@gmail.com>
2023-08-23 21:30:01 +0800

    test

--- wp-content/themes/tulisandaiyan/template-part/footer-content.php
+++ wp-content/themes/tulisandaiyan/template-part/footer-content.php
@@ -1,5 +1,5 @@
 <footer class="bg-[#282828]">
   <div>
-    <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/white-logo.png" alt="" class="mx-auto w-[120px]">
+    <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/white-logo.png" alt="" class="mx-auto w-[130px]">
   </div>
 </footer>
