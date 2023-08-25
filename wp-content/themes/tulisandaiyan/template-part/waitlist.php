c56b70d11eb94748c6951db69e3163e05b5736d5
Made Supardi <grymore@gmail.com>
2023-08-25 18:51:15 +0800

    change theme

--- wp-content/themes/tulisandaiyan/template-part/waitlist.php
+++ wp-content/themes/tulisandaiyan/template-part/waitlist.php
@@ -1,13 +1,13 @@
-<div id="contactForm" class="bg-[#8E2424] py-16 sm:py-24 px-4">
+<div id="contactForm" class="bg-[#F3F2EC] py-16 sm:py-24 px-4">
   <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
     <div class="relative isolate">
-      <h2 class="mx-auto max-w-2xl text-center font-bold tracking-tight text-white font-butler text-[32px] md:text-[48px] font-medium ">Join The Waitlist!</h2>
-      <p class="mx-auto mt-2 max-w-3xl text-center text-lg text-gray-300 font-light">Oh no, the current pre-order of 'Tulisan' has officially sold out!
+      <h2 class="mx-auto max-w-2xl text-center font-bold tracking-tight font-butler text-[32px] md:text-[48px] font-medium ">Join The Waitlist!</h2>
+      <p class="mx-auto mt-2 max-w-3xl text-center text-[#74642F] text-lg text-gray-300 font-light">Oh no, the current pre-order of 'Tulisan' has officially sold out!
         But no worries, the subsequent print is already in the work.
         Join our waitlist to be among the first to know about the
         reprint updates and enjoy a 10% off voucher* !
       </p>
-      <p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300 font-bold">* valid only for the first 1000 waitlisted customers</p>
+      <p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-[#000000] font-bold">* valid only for the first 1000 waitlisted customers</p>
       <div class="container-form"><?php echo do_shortcode(get_field('waitlist'))?></div>
     </div>
   </div>
