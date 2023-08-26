bdb2ce52e9176a494a980df263af7a39f9f156ed
Made Supardi <grymore@gmail.com>
2023-08-26 10:05:19 +0800

    update text

--- wp-content/themes/tulisandaiyan/template-part/waitlist.php
+++ wp-content/themes/tulisandaiyan/template-part/waitlist.php
@@ -2,10 +2,11 @@
   <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
     <div class="relative isolate">
       <h2 class="mx-auto max-w-2xl text-center font-bold tracking-tight font-butler text-[32px] md:text-[48px] font-medium ">Join The Waitlist!</h2>
-      <p class="mx-auto mt-2 max-w-3xl text-center text-[#74642F] text-lg font-light">Oh no, the current pre-order of 'Tulisan' has officially sold out!
+      <p class="mx-auto mt-2 max-w-3xl text-center text-[#74642F] text-lg font-light">
+        Oh no, the current pre-order of 'Tulisan' has officially sold out!
         But no worries, the subsequent print is already in the work.
         Join our waitlist to be among the first to know about the
-        reprint updates and enjoy a 10% off voucher* !
+        reprint updates!
       </p>
       <p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-[#000000] font-bold">* valid only for the first 1000 waitlisted customers</p>
       <div class="container-form"><?php echo do_shortcode(get_field('waitlist'))?></div>
